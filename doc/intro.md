1e307828696716acd0904091939987312c3c961d
Dylon Edwards <dylon.edwards@gmail.com>
2013-06-09 11:43:42 -0700

    Initialized project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to umlang
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/umlang/core_test.clj
@@ -0,0 +1,7 @@
+(ns umlang.core-test
+  (:require [clojure.test :refer :all]
+            [umlang.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
