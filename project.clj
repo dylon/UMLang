4b558477ac5e056719287f02e309e8f702950daa
Dylon Edwards <dylon.edwards@gmail.com>
2013-06-09 13:01:52 -0700

    Updates the project info and adds initial logging capabilities

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject umlang "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+(defproject umlang "0.1.0-alpha"
+  :description "Symbolic/Numeric CAS (Computer Algebra System) with UML-like Syntax"
+  :url "https://github.com/dylon/umlang"
+  :license {:name "The MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.antlr/antlr4 "4.0"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :main umlang.core)
