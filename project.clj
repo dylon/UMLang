ad9617f82782c2989ef6adb2e2f058d1eef552c2
Dylon Edwards <dylon.edwards@gmail.com>
2013-06-09 13:17:54 -0700

    Reorganizes the src/ directory to support multiple languages

--- project.clj
+++ project.clj
@@ -1,3 +1,23 @@
+;; Copyright (c) 2013 Dylon Edwards <dylon.edwards@gmail.com>
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 (defproject umlang "0.1.0-alpha"
   :description "Symbolic/Numeric CAS (Computer Algebra System) with UML-like Syntax"
   :url "https://github.com/dylon/umlang"
@@ -11,4 +31,7 @@
                                                     com.sun.jmx/jmxri]]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.clojure/tools.logging "0.2.6"]]
+  :plugins [[org.antlr/stringtemplate "4.0.7"]]
+  :source-paths ["src/clojure/"]
+  :java-source-paths ["src/java/"]
   :main umlang.core)
